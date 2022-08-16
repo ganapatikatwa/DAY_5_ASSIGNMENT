df5441be71052a3437ebf34dc3c3eaebf2981e28
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-16 18:24:04 +0530

    [Ganapati]Flip Coin

---
+++ C_Programming/Flip_Coin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Programming
+{
+    public class Flip_Coin
+    {
+        public void FlipCoin()
+        {
+            Console.WriteLine("Enter How many times to flip coin");
+            double n = Double.Parse(Console.ReadLine());
+
+
+            int h_count = 0;
+            int t_count = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Random random = new Random();
+                int head_or_tail = random.Next(0, 2);
+                if (head_or_tail == 1)
+                {
+                    Console.WriteLine("Heads");
+                    h_count++;
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                    t_count++;
+                }
+            }
+            Console.WriteLine("no of heads " + h_count);
+            Console.WriteLine("no of tails " + t_count);
+
+            double Head_percentage = (h_count / n) * 100;
+            double Tail_Percentage = (t_count / n) * 100;
+
+            Console.WriteLine("Percentage of Heads is = {0}%", Head_percentage);
+            Console.WriteLine("Percentage of Tails is = {0}% ", Tail_Percentage);
+
+        }
+    }
+}
---
+++ C_Programming/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_Programming
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            {
+                Flip_Coin obj = new Flip_Coin();
+
+
+                Console.WriteLine("Flip coin problem");
+                obj.FlipCoin();
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+            }
+        }
+    }
+}
